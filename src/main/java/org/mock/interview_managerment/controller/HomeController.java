e5003d9631095687c5e5b885a84520520a0a1f99
nguyencongvan <vannguyencong2002@gmail.com>
2024-07-25 10:26:43 +0700

    Hoan thien login

--- src/main/java/org/mock/interview_managerment/controller/HomeController.java
+++ src/main/java/org/mock/interview_managerment/controller/HomeController.java
@@ -1,17 +1,14 @@
 package org.mock.interview_managerment.controller;
 
-import org.mock.interview_managerment.entities.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class LoginController {
+public class HomeController {
     @GetMapping("/home")
     public String viewHomeUser(Model model) {
         getUserInfor(model);

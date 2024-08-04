5383ee9fef1f6ed1232bcf735c6d6eef20159537
B20DCAT186TranDangThuc <trthucpro@gmail.com>
2024-08-05 01:20:41 +0700

    custom disabled user login

--- src/main/java/org/mock/interview_managerment/services/CustomUserDetailsService.java
+++ src/main/java/org/mock/interview_managerment/services/CustomUserDetailsService.java
@@ -24,13 +24,19 @@ public class CustomUserDetailsService implements UserDetailsService {
 
         User userx = user.orElseThrow(() -> new UsernameNotFoundException(username));
 
-        if (user == null) {
-            throw new UsernameNotFoundException(username);
-        }
+        boolean enabled = userx.getStatus().name().equals("ACTIVE");
+
+        boolean accountNonExpired = true; // Giả sử tài khoản không hết hạn
+        boolean credentialsNonExpired = true; // Giả sử thông tin xác thực không hết hạn
+        boolean accountNonLocked = true; // Giả sử tài khoản không bị khóa
 
         return new org.springframework.security.core.userdetails.User(
                 userx.getUsername(),
                 userx.getPassword(),
+                enabled,
+                accountNonExpired,
+                credentialsNonExpired,
+                accountNonLocked,
                 Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + userx.getRole().getRoleName()))
         );
     }
